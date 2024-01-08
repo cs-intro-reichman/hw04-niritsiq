590d59e3437339e0204e03e06280769742b270b1
niritsiq <154353994+niritsiq@users.noreply.github.com>
2024-01-08 19:08:04 +0200

    Add files via upload

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,94 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
-    }
-    
-    public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int[] array1 = { 3, -4, 1, 2, 5 };
+        int[] array2 = { 1, 3, 2 };
+        System.out.println(isSorted(array2));
+
     }
 
-    public static int secondMaxValue(int [] array) {
+    public static int findMissingInt(int[] array) {
         // Write your code here:
-        return 0;
+        boolean check = false;
+        int missing = 0, i = 0;
+        if (array.length == 1)
+            return 1;
+        while (i < array.length) {
+            for (int j = 0; j < array.length; j++) {
+                if (i == array[j]) {
+
+                    check = true;
+                }
+            }
+            if (check) {
+                missing = i;
+            }
+            i++;
+        }
+
+        return missing;
     }
 
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
+    public static int secondMaxValue(int[] array) {
+
         // Write your code here:
+        int count = 0;
+        int secondMax = array[0], max = array[0];
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] > max)
+                max = array[i];
+        }
+
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] > secondMax && array[j] < max)
+                secondMax = array[j];
+            if (array[j] == max)
+                count++;
+
+        }
+        if (count > 1)
+            return max;
+
+        return secondMax;
+    }
+
+    public static boolean exist(int[] array, int x) {
+        for (int i = 0; i < array.length; i++) {
+            if (x == array[i])
+                return true;
+        }
         return false;
     }
 
-    public static boolean isSorted(int [] array) {
+    public static boolean containsTheSameElements(int[] array1, int[] array2) {
         // Write your code here:
-        return false;
+
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (!exist(array2, array1[j])) {
+                    return false;
+                }
+                if (!exist(array1, array2[j]))
+                    return false;
+
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isSorted(int[] array) {
+        // Write your code here:
+
+        for (int i = 0; i < array.length - 2; i++) {
+
+            if (array[i] < array[i + 1] && array[i + 1] > array[i + 2])
+                return false;
+            if (array[i] > array[i + 1] && array[i + 1] < array[i + 2])
+                return false;
+
+        }
+        return true;
     }
 
 }
