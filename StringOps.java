8971f174287d3e9dff12202dccf16586936c5f4b
niritsiq <154353994+niritsiq@users.noreply.github.com>
2024-01-11 09:03:59 +0200

    Add files via upload

--- StringOps.java
+++ StringOps.java
@@ -23,26 +23,44 @@ public class StringOps {
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
         String str = args[0];
+
         System.out.println(capVowelsLowRest(str));
+    }
+
+    public static char changeToLower(char c) {
+        if (c >= 'A' && c <= 'Z')
+            return (char) (c + ('a' - 'A'));
+        else
+            return c;
+
     }
 
     public static String capVowelsLowRest(String str) {
         // Write your code here:
 
         String newS = "";
-        for (int i = 0; i < str.length(); i++) {
 
+        for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if ("aeiou".indexOf(c) != -1) {
+            if (i == 0 && "AEIOU".indexOf(c) != -1) {
+
+                newS += str.charAt(i);
+
+            } else if ("aeiou".indexOf(c) != -1) {// בודק אם יש אות ניקוד קטנה ואם כן אז הופך
+
                 newS += (char) (c - 32);
-            } else if ((int) c >= 65 && (int) c <= 90 && !("aeiou".indexOf(c) != -1)) {
+            } else if ("AEIOU".indexOf(c) != -1) {// בודק אם יש אות ניקוד גדולה ואם כן אז
+
                 newS += (char) (c + 32);
-            } else if ((int) c >= 65 && (int) c <= 90)
+            } else if ((int) c >= 65 && (int) c <= 90)// בודק אם אותיות גדולות ולא ניקוד
+
                 newS += (char) (c + 32);
-            else
-                newS += c;
+
+            else// אם האות קטנה ולא ניקוד תשאיר אותה ככה
+                newS += changeToLower(str.charAt(i));
 
         }
+
         return newS;
     }
 
